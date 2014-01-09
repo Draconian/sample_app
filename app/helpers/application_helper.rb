dcac486195f61065bfaa13bd249ce63dc0ef1810
Francois Dufault <dufault.f@videotron.ca>
2014-01-08 20:57:29 -0500

    Classe User

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+	 # Retourner un titre basé sur la page.
+  def titre
+    base_titre = "Simple App du Tutoriel Ruby on Rails"
+    if @titre.nil?
+      base_titre
+    else
+      "#{base_titre} | #{@titre}"
+    end
+  end
+
 end
